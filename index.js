b7e0bc2b2b137031106d2ab1d32df318b04a518c
Shreya-og <shreyachaturved@gmail.com>
2025-08-21 20:40:43 +0530

    change for deployment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 import sql from './db.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const saltRounds = 10;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
